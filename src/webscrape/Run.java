abd6815e157273d69b3ca616ac3bc582c0650721
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-03-12 12:24:44 -0700

    Fixed the missing duplicate issue.
    My compare extension was not of the right class. Switched to a a List, which can handle multiple keys having the same value.

--- src/webscrape/Run.java
+++ src/webscrape/Run.java
@@ -2,12 +2,13 @@ package webscrape;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
+import java.util.Map.Entry;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,7 +17,6 @@ import org.jsoup.select.Elements;
 
 //https://siftscience.com/jobs#engineer
 
-//email to: jobs+frontend+b64@siftscience.com
 
 public class Run {
 	//Assuming that we can have an infinite set of valid letters.
@@ -24,9 +24,6 @@ public class Run {
 	public static void main(String[] args) {
 		//Create a wordlist from the given site and tag.
 		ArrayList<String> wordlist = scrape("http://www.giwersworld.org/computers/linux/common-words.phtml", "pre");
-		//ArrayList<String> wordlist = scrape("http://phrontistery.info/a.html", "td");
-		
-		
 		
 		
 		for(int i = 0; i < wordlist.size(); ++i){
@@ -34,32 +31,52 @@ public class Run {
 		}
 		
 		//Get a value ordered SortedSet from the TreeMap.
-		SortedSet orderedList = valSort(counter);
+		List<Entry<String, Integer>> orderedList = valSort(counter);
 		counter = null;
 		
 		//Iterate through the orderedList and print the 10 most frequent letters.
-		int i = 0;
-		Iterator it = orderedList.iterator();
-		while(i < 10 && it.hasNext()){
+		Iterator<Entry<String, Integer>> it = orderedList.iterator();
+		for(int i = 0; i < 10 && it.hasNext(); ++i){
 			Object element = it.next();
 			System.out.println(element.toString());
-			++i;
 		}
 		
 	}
 	
-	static <K,V extends Comparable<? super V>> SortedSet<Map.Entry<K,V>> valSort(Map<K,V> map) {
-	    SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
-	        new Comparator<Map.Entry<K,V>>() {
-	            @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
-	                return e2.getValue().compareTo(e1.getValue());
-	            }
+	/**
+	 * Method valSort
+	 * Extends generic TreeSet functionality to sort by the value, as opposed
+	 * to the key. Then return a SortedSet of generic Map Entries.
+	 * 
+	 * @param map 
+	 * @return SortedSet sorted by value
+	 */
+	static <K,V extends Comparable<? super V>> List<Entry<K, V>> valSort(Map<K,V> map) {
+
+		List<Entry<K,V>> sortedEntries = new ArrayList<Entry<K,V>>(map.entrySet());
+	
+		Collections.sort(sortedEntries, new Comparator<Entry<K,V>>() {
+	        @Override
+	        public int compare(Entry<K,V> e1, Entry<K,V> e2) {
+	            return e2.getValue().compareTo(e1.getValue());
 	        }
-	    );
-	    sortedEntries.addAll(map.entrySet());
-	    return sortedEntries;
+		}
+		);
+
+		return sortedEntries;
 	}
 	
+	/**
+	 * Method scrape
+	 * Goes to a given site, looks for the given tag, and puts newline-
+	 * separated text into an ArrayList for further processing. Also removes
+	 * empty lines and any tags.
+	 * 
+	 * @param url (String) URL of the target site.
+	 * @param tag (String) Dictates which HTML tags to look for when parsing
+	 * 	text.
+	 * @return ArrayList<String> filled with newline-delimited words/phrases.
+	 */
 	private static ArrayList<String> scrape(String url, String tag){
 		ArrayList<String> wordlist = new ArrayList<String>();
 		Document doc;
@@ -81,8 +98,7 @@ public class Run {
 		//Remove whitespace lines.
 		list = list.replaceAll("(?m)^[ \t]*\r?\n", "");
 		
-		//Debug to show what I'm parsing.
-		//System.out.println(list);
+		//System.out.println(list); //DEBUG
 		
 		//Split the string by newlines and put it into the returned ArrayList.
 		String temp[] = list.split("\\r?\\n");
@@ -96,7 +112,7 @@ public class Run {
 	
 	/**
 	 * Method dissectWord
-	 * Adds 
+	 * Adds the letters of a given word to the counter map.
 	 * 
 	 * @param word
 	 */
